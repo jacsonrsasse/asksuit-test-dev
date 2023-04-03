5a1833a4afa1e6c5a4fe97ab55f718cb13f52cdb
Jacson <jacson.sasse@hotmail.com>
2023-04-03 02:24:02 -0300

    Movida a lógica para dentro do Service

--- src/controller/SearchsController.js
+++ src/controller/SearchsController.js
@@ -6,67 +6,10 @@ moment.locale("pt-br");
 
 class SearchsController {
     search = async (req, res) => {
-        const url = this.getUrl(req);
-
-        const browser = await BrowserService.getBrowser();
-        const page = await browser.newPage();
-        await page.goto(url);
-
-        try {
-            await page.waitForSelector("#tblAcomodacoes");
-
-            const results = await page.evaluate(() => {
-                const nodeList = document.querySelectorAll(".row-quarto");
-                const rooms = [...nodeList];
-                const results = rooms.map((room) => {
-                    const roomInfo = room.querySelector(".tdQuarto");
-                    const roomDetails = room.querySelector(".tdContainer");
-
-                    return {
-                        name: roomInfo.querySelector(".quartoNome").innerText,
-                        description:
-                            roomInfo.querySelector(".quartoDescricao")
-                                .innerText,
-                        price: roomDetails
-                            .querySelector(".precoQuarto")
-                            .querySelector(".valorFinal").innerText,
-                        image:
-                            roomInfo.querySelector("img").src ||
-                            roomInfo
-                                .querySelector("img")
-                                .getAttribute("data-src"),
-                    };
-                });
-
-                return results;
-            });
-
-            await BrowserService.closeBrowser(browser);
-
-            return res.status(200).json(results);
-        } catch (error) {
-            throw new Exception(
-                "It looks like your request is taking too long to return. Please, try again.",
-                408
-            );
-        }
+        const service = new BrowserService();
+        const result = await service.getResult(req);
+        return res.status(200).json(result);
     };
-
-    getUrl = (req) => {
-        const body = req.body;
-        const checkin = moment(body.checkin, "YYYY-MM-DD").format("L");
-        const checkout = moment(body.checkout, "YYYY-MM-DD").format("L");
-
-        const url = (process.env.URL_WEB_CRAWLING ?? SearchsController.url())
-            .replace("{checkin_value}", checkin)
-            .replace("{checkout_value}", checkout);
-
-        return url;
-    };
-
-    static url() {
-        return "https://pratagy.letsbook.com.br/D/Reserva?checkin={checkin_value}&checkout={checkout_value}&cidade=&hotel=12&adultos=2&criancas=&destino=Pratagy+Beach+Resort+All+Inclusive&promocode=&tarifa=&mesCalendario=3%2F14%2F2023";
-    }
 }
 
 module.exports = SearchsController;
--- src/services/BrowserService.js
+++ src/services/BrowserService.js
@@ -1,7 +1,13 @@
 require("dotenv").config();
+var moment = require("moment");
+moment.locale("pt-br");
 const puppeteer = require("puppeteer");
+const Exception = require("../error/Exception");
 
 class BrowserService {
+    static url =
+        "https://pratagy.letsbook.com.br/D/Reserva?checkin={checkin_value}&checkout={checkout_value}&cidade=&hotel=12&adultos=2&criancas=&destino=Pratagy+Beach+Resort+All+Inclusive&promocode=&tarifa=&mesCalendario=3%2F14%2F2023";
+
     static getBrowser() {
         return puppeteer.launch();
     }
@@ -11,6 +17,65 @@ class BrowserService {
             return;
         }
         return browser.close();
+    }
+
+    async getResult(req) {
+        const url = BrowserService.createRequestUrl(req);
+
+        const browser = await BrowserService.getBrowser();
+        const page = await browser.newPage();
+        await page.goto(url);
+
+        try {
+            await page.waitForSelector("#tblAcomodacoes");
+
+            const results = await page.evaluate(() => {
+                const nodeList = document.querySelectorAll(".row-quarto");
+                const rooms = [...nodeList];
+                const results = rooms.map((room) => {
+                    const roomInfo = room.querySelector(".tdQuarto");
+                    const roomDetails = room.querySelector(".tdContainer");
+
+                    return {
+                        name: roomInfo.querySelector(".quartoNome").innerText,
+                        description:
+                            roomInfo.querySelector(".quartoDescricao")
+                                .innerText,
+                        price: roomDetails
+                            .querySelector(".precoQuarto")
+                            .querySelector(".valorFinal").innerText,
+                        image:
+                            roomInfo.querySelector("img").src ||
+                            roomInfo
+                                .querySelector("img")
+                                .getAttribute("data-src"),
+                    };
+                });
+
+                return results;
+            });
+
+            await BrowserService.closeBrowser(browser);
+
+            return results;
+        } catch (error) {
+            throw new Exception(
+                "It looks like your request is taking too long to return. Please, try again.",
+                408
+            );
+        }
+    }
+
+    static createRequestUrl(req) {
+        const body = req.body;
+        const checkin = moment(body.checkin, "YYYY-MM-DD").format("L");
+        const checkout = moment(body.checkout, "YYYY-MM-DD").format("L");
+
+        const url = (process.env.URL_WEB_CRAWLING ?? BrowserService.url)
+            .replace("{checkin_value}", checkin)
+            .replace("{checkout_value}", checkout);
+
+        return url;
     }
 }
 
